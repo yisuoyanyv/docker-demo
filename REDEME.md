adfc2282f8a4bef87ee62b2a27c2a035bcff5a46
yisuoyanyv <840834298@qq.com>
2022-06-01 17:45:48 +0800

    minio spark  docker-compose部署

---
+++ REDEME.md
@@ -0,0 +1,10 @@
+参考文档：
+
+https://www.jianshu.com/p/d6a406da3cba
+
+
+```
+docker run --rm -it --network spark-network jinglongzh/spark:3.2.1 /bin/sh
+
+docker run --rm -it  jinglongzh/spark:3.2.1 /bin/sh
+```
---
+++ spark-docker/scripts/start-shell.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source /root/.bashrc
+
+export SPARK_MASTER_HOST=`hostname`
+
+mkdir -p $SPARK_MASTER_LOG
+
+export SPARK_HOME=/spark
+
+ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
+
+spark-class org.apache.spark.deploy.master.Master \ 
+    --ip $SPARK_MASTER_HOST \
+    --port  $SPARK_MASTER_PORT \ 
+    --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG/spark-master.out
